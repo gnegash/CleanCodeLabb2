92c07b20df0d59c65109bcb2d893ad932aa9b578
Geweria <geweria.negash@iths.se>
2024-12-18 10:43:45 +0100

    dotnet

--- UserService/Program.cs
+++ UserService/Program.cs
@@ -22,43 +22,43 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-    var logger = services.GetRequiredService<ILogger<Program>>();
-    var dbContext = services.GetRequiredService<MicroServiceDbContext>();
+//using (var scope = app.Services.CreateScope())
+//{
+//    var services = scope.ServiceProvider;
+//    var logger = services.GetRequiredService<ILogger<Program>>();
+//    var dbContext = services.GetRequiredService<MicroServiceDbContext>();
 
-    var maxRetries = 5;
-    var delayMilliseconds = 2000;
-    var connected = false;
+//    var maxRetries = 10;
+//    var delayMilliseconds = 2000;
+//    var connected = false;
 
-    for (int i = 0; i < maxRetries; i++)
-    {
-        try
-        {
-            logger.LogInformation("Försöker ansluta till databasen... Försök {Attempt}", i + 1);
-            await dbContext.Database.OpenConnectionAsync();
-            logger.LogInformation("Anslutning till databasen lyckades.");
-            await dbContext.Database.CloseConnectionAsync();
-            connected = true;
-            break;
-        }
-        catch (Exception ex)
-        {
-            logger.LogWarning(ex, "Misslyckades med att ansluta till databasen. Försöker igen om {Delay} ms...", delayMilliseconds);
-            await Task.Delay(delayMilliseconds);
-        }
-    }
+//    for (int i = 0; i < maxRetries; i++)
+//    {
+//        try
+//        {
+//            logger.LogInformation("Försöker ansluta till databasen... Försök {Attempt}", i + 1);
+//            await dbContext.Database.OpenConnectionAsync();
+//            logger.LogInformation("Anslutning till databasen lyckades.");
+//            await dbContext.Database.CloseConnectionAsync();
+//            connected = true;
+//            break;
+//        }
+//        catch (Exception ex)
+//        {
+//            logger.LogWarning(ex, "Misslyckades med att ansluta till databasen. Försöker igen om {Delay} ms...", delayMilliseconds);
+//            await Task.Delay(delayMilliseconds);
+//        }
+//    }
 
-    if (!connected)
-    {
-        logger.LogError("Kunde inte ansluta till databasen efter {MaxRetries} försök.", maxRetries);
-        throw new Exception("Kunde inte ansluta till databasen.");
-    }
+//    if (!connected)
+//    {
+//        logger.LogError("Kunde inte ansluta till databasen efter {MaxRetries} försök.", maxRetries);
+//        throw new Exception("Kunde inte ansluta till databasen.");
+//    }
 
-    // Utför eventuella databasoperationer, t.ex. migreringar
-    dbContext.Database.Migrate();
-}
+//    // Utför eventuella databasoperationer, t.ex. migreringar
+//    dbContext.Database.Migrate();
+//}
 
 // Configure the HTTP request pipeline.
 
